6462ab925e50320b073ee2d3327ddfbf4fb644db
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-02-13 13:22:36 -0300

    Criação de controller e rota para listar os horários de agendamento livre

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controller/ProviderController';
 import AppointmentController from './app/controller/AppointmentController';
 import ScheduleController from './app/controller/ScheduleController';
 import NotificationController from './app/controller/NotificationController';
+import AvailableController from './app/controller/AvailableController';
 import authMiddleware from './app/middleware/auth';
 import multerConfig from './config/multer';
 
@@ -20,7 +21,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
