72afbe5a1157818f13fb04cff9e326bbb16735ce
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-02-14 13:37:03 -0300

    Adicionando o Sentry para controle de exceções

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import path from 'path';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
 import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -19,7 +25,16 @@ class App {
   }
 
   routes() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://8e6c2dc1526843f2a1d15794fa4d3f5d@sentry.io/2522402',
+};
